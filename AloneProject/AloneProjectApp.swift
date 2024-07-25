be2ae0c1e24d355ef5e7dd37bb8d268c28b9aae5
곽현우 <polla723@naver.com>
2024-07-25 14:33:58 +0900

    Initial Commit

---
+++ AloneProject/AloneProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AloneProjectApp.swift
+//  AloneProject
+//
+//  Created by 곽현우 on 7/25/24.
+//
+
+import SwiftUI
+
+@main
+struct AloneProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
